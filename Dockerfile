7633df8168fa1e5d547002945233953464785dec
Fabian Jakub <fabian@jakub.com>
2016-03-28 06:50:43 +0000

    add 3 javascript sliders to update color upon button press

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,8 @@ FROM resin/rpi-raspbian
 ENV PYTHONUNBUFFERED 1
 RUN mkdir /code
 WORKDIR /code
-RUN apt-get update  && apt-get install -y  python-dev python-pip python-psycopg2 python-numpy
+RUN apt-get update  && apt-get install -y  python-dev python-pip python-psycopg2 python-numpy build-essential netcat
 ADD requirements.txt /code/
 RUN pip install -r requirements.txt
 ADD docker-compose.yml /code/
 ADD Dockerfile /code/
-RUN apt-get install -y build-essential && pip install spidev
--- lampserver/views.py
+++ lampserver/views.py
@@ -6,10 +6,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def lamp_view(request, R,B,G):
-    color = [R,B,G,255]
+    color = [ int(x) for x in [R,B,G,255]]
     with transaction.atomic():
         for l in LEDLamp.objects.order_by('id'):
             l.lamp_values = color
             l.save()
-    html = "<html><body>It is now %s.</body></html>" % color
-    return HttpResponse(html)
+    context = {'color':color,}
+    return render(request, 'lampserver/lamp_view.html', context)
