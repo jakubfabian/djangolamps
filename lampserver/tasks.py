c10bbe77957e8deb7b979edf3d6c5097ad30f40a
Fabian Jakub <fabian@jakub.com>
2016-03-30 03:25:34 +0000

    add usage of alpha channel, moved slider to jquerymobile slider and change constant loop of SPI worker to only run one msg

--- lampserver/tasks.py
+++ lampserver/tasks.py
@@ -3,14 +3,21 @@ from __future__ import absolute_import
 from celery import shared_task
 import time
 import spidev
+from lampserver.models import LEDLamp
+
+
+SPI=None
+
 
 def spi_send(spi, lamps):
     v = lamp_values(lamps)
     return spi.xfer(v)
 
+
 def gamma_correct(inp):
     gamma = 2.8
     return int( (inp/255.)**gamma * 255 + .5)
+
 
 def lamp_values(lamps):
         start = [0]*4
@@ -18,34 +25,36 @@ def lamp_values(lamps):
         v = []
         for l in lamps:
                 R,G,B,A = [ gamma_correct(i) for i in l ]
-                #brightness = [ (7<<5)| int(A/255.*7) ] # first 4 ones and then 63 is full brightness dimming
-                brightness = [ (7<<5)| 63 ] # first 4 ones and then 63 is full brightness dimming
-                brightness = [ (7<<5)| 1 ] # first 4 ones and then 63 is full brightness dimming
+                brightness = [ (15<<5)| 63 ] # first 4 ones and then 63 is full brightness dimming
+                brightness = [ (15<<5)| 1 ] # first 4 ones and then 63 is full brightness dimming
+                brightness = [ (15<<5)| int(A/255.*15+.5) ] # first 4 ones and then 63 is full brightness dimming
                 v+=brightness+[B,G,R]
+        print 'brightness', brightness
 
         return start+v+end
 
-@shared_task
+
+@shared_task(ignore_result=True)
 def update_lamps():
-    print('Started update_lamps worker')
-    time.sleep(1) # wait till everything is up and running
-    from lampserver.models import LEDLamp
-    lamps = LEDLamp.objects.all()
+    global SPI
 
     Nlamps = 300
 
+    print('Started update_lamps worker')
+    lamps = LEDLamp.objects.all()
+
     if len(lamps)!=Nlamps:
         [ l.delete() for l in lamps ]
-        [ LEDLamp(id=i, lamp_values=[255,255,255,255]).save() for i in range(300) ]
+        [ LEDLamp(id=i, lamp_values=[255,255,255,255]).save() for i in range(Nlamps) ]
 
-    spi = spidev.SpiDev()
-
-    spi.open(0,0)
-    spi.max_speed_hz = 1250000*4
+    if SPI is None:
+	    SPI = spidev.SpiDev()
+	    SPI.open(0,0)
+	    SPI.max_speed_hz = 1250000*4
     
-    while True:
-        lamp_vals = [ l.lamp_values for l in LEDLamp.objects.order_by('id') ]
-	spi_send(spi, lamp_vals)
-	time.sleep(1e-2)
+    #while True:
+    lamp_vals = [ l.lamp_values for l in LEDLamp.objects.order_by('id') ]
+    spi_send(SPI, lamp_vals)
+    #time.sleep(1e-2)
         
     return
--- lampserver/views.py
+++ lampserver/views.py
@@ -3,6 +3,7 @@ from django.db import transaction
 from lampserver.models import LEDLamp
 
 from django.http import HttpResponse
+from .tasks import update_lamps
 
 # Create your views here.
 def lamp_view(request, **kwargs):
@@ -15,11 +16,15 @@ def lamp_view(request, **kwargs):
         color[1] = kwargs['G']
     if 'B' in kwargs:
         color[2] = kwargs['B']
+    if 'A' in kwargs:
+        color[3] = kwargs['A']
 
     with transaction.atomic():
         for l in LEDLamp.objects.order_by('id'):
             l.lamp_values = color
             l.save()
+    print 'Current color to be set:', LEDLamp.objects.order_by('id')[0].lamp_values
+    update_lamps()
     context = {'color':color,}
 
     if request.is_ajax():
